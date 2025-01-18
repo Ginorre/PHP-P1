d518de9de6474dd8705fd8eb6b97b1d067cc9af7
Ginorre <149720785+Ginorre@users.noreply.github.com>
2025-01-18 15:00:24 +0100

    opdracht 2 opdracht 2
    Forum opdracht

--- week2/contact.php
+++ week2/contact.php
@@ -4,6 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>contact</title>
+
+    <style>
+        button {
+            cursor: pointer;
+        }
+    </style>
 </head>
 <body>
 <?php
@@ -11,7 +17,17 @@
     ?>
 
     <h1>dit is een contact page</h1>
-    
+    <form action="contact.php">
+        <label for="fname">First Name</label><br>
+        <input type="text" id="fname" name="fname"><br>
+        <label for="lname">Last name</label><br>
+        <input type="text" name="lname" id="lname"><br>
+        <label for="email">Email</label><br>
+        <input type="text" name="email" id="email"><br><br>
+        <label for="Message">Message Us!</label><br><br>
+        <textarea name="Message" id="Message" style="width: 200px; height:300px;">Write Message Here</textarea><br>
+        <button type="button" onclick="alert('Message Sent')">Send Message</button>
+    </form>
     <?php
     include 'footer.php'
     ?>
