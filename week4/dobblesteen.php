2b9c5ac4e327e58e4791de34300af8068719e4d5
Ginorre <149720785+Ginorre@users.noreply.github.com>
2025-01-18 16:03:55 +0100

    DOBBLESTEEN
    WEEK4 DOBBLESTEEN OPDRACHT

--- week4/dobblesteen.php
+++ week4/dobblesteen.php
@@ -1,10 +1,34 @@
-<?php
-$score = 3;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dobbelsteen spel</title>
+    <script>
+       
+        function gooiDobbelsteen() {
+            fetch('dobblesteen.php') 
+                .then(response => response.text()) 
+                .then(data => {
+                    document.getElementById('resultaat').innerHTML = data;
+                });
+        }
+    </script>
+</head>
+<body>
 
-if ($score > 3) { 
-    echo " Haha, You lost !";
+<div id="resultaat">
+    <?php
+$randomGetal = rand(1,6);
+echo "<br>Je hebt gegooid: $randomGetal;";
+
+if ($randomGetal == 6) {
+    echo " Congrats, You Won ";
 } else {
-    echo "Good job, You Won !";
+    echo " Awh, You Lost Noobie ";
 }
-$randomGetal =rand(1,6)
 ?>
+</div>
+    <button onclick="gooiDobbelsteen()">Try Again!</button>
+</body>
+</html>
