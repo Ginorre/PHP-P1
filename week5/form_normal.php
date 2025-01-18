de14925402fd7a286be2bdefe6e1a938711c39b4
Ginorre <149720785+Ginorre@users.noreply.github.com>
2025-01-18 22:25:32 +0100

    asda
    asdad

---
+++ week5/form_normal.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vliegtuigen</title>
+</head>
+
+<body>
+    <form action="" method="POST">
+        <label for="name">Naam vliegtuig</label>
+        <input type="text" id="name" name="name">
+
+        <label for="available">Ja</label>
+        <input type="radio" id="available" name="available" value="Ja">
+
+        <label for="available">Nee</label>
+        <input type="radio" id="available" name="available" value="Nee">
+
+    </form>
+    
+</body>
+</html>
